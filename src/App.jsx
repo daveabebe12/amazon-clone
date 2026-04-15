2e7c6916f368d5dd5994d9329494d90e43365cff
Dawit Abebe <davo12ab@gmail.com>
2026-04-15 14:08:25 +0300

    Initial commit: Initialize React project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+// import header from './components/Header/header'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function header() {
+  return (
+    <section>
+        <section>
+            <div>
+                <a href="">
+                    <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
+                </a>
+            </div>
+            <div></div>
+            <div></div>
+        </section>
+    </section>
+  )
+}
+export default header;
