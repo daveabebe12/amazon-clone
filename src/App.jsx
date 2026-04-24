b9d81b306fa38c939a44a3e1d888878f395a3b51
Dawit Abebe <davo12ab@gmail.com>
2026-04-24 22:18:33 +0300

    Responsive
    added @media to make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Header from './components/Header/header'
-
+import Carousel from './components/Carousel/Carousel'
 function App() {
 
   return (
     <div>
         <Header/>
+        <Carousel/>
     </div>
   )
 }
