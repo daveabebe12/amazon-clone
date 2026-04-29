f29224c108ea221f395cebb08924fcf660f2c877
Dawit Abebe <davo12ab@gmail.com>
2026-04-29 21:57:22 +0300

    category added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Header from './components/Header/header'
 import Carousel from './components/Carousel/Carousel'
+import Category from './components/Category/category'
+
 function App() {
 
   return (
     <div>
         <Header/>
         <Carousel/>
+        <Category/>
     </div>
   )
 }
---
+++ src/components/Category/category.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CategoryCard from './categoryCard'
+import {categoryInfo} from './categoryFullInfo'
+import classes from './category.module.css'
+
+function Category() {
+  return (
+    <section className={classes.category_container}>
+      {
+        categoryInfo.map((infos) => (
+          <CategoryCard key={infos.name} data={infos} />
+        ))
+      }
+    </section>
+  )
+}
+
+export default Category;
